49950bd613625d05a41167bb80569d932d293561
LAPTOP-BTT3NSUQ\qotn0 <qotn0327@naver.com>
2024-04-01 19:44:34 +0900

    [배수빈] - git 연동

---
+++ src/main/java/com/bsoupb/sushisushi/SushisushiApplication.java
@@ -0,0 +1,13 @@
+package com.bsoupb.sushisushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushisushiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushisushiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsoupb/sushisushi/SushisushiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsoupb.sushisushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushisushiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
